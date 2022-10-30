96b1175cc10924cd82779b2f35dbe6a5f2ed01a0
christopher rinehart <RInehartC20@gmail.com>
2022-10-29 22:15:23 -0400

    Initial commit

---
+++ Assets/GameBoard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Holds the basic UI calls for the game board
+public class GameBoard : MonoBehaviour
+{
+    public GameObject userBoard; 
+    public GameObject opponentBoard;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PlayBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//creates the game board on startup
+public class PlayBoard : MonoBehaviour
+{
+    // clickable ocean tiles
+    public GameObject tiles;
+    //prefab for tiles
+    public GameObject label;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
