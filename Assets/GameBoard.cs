b202d921cc135a6aa60dd8d5aeb84e475d8a949c
christopher rinehart <RInehartC20@gmail.com>
2022-10-30 23:44:11 -0400

    Updated from gameobject to Playboard to call for the script we are using

--- Assets/GameBoard.cs
+++ Assets/GameBoard.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 // Holds the basic UI calls for the game board
 public class GameBoard : MonoBehaviour
 {
-    public GameObject userBoard; 
-    public GameObject opponentBoard;
+    public PlayBoard userBoard; 
+    public PlayBoard opponentBoard;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
