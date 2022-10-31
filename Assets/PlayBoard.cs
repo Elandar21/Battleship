6ea88973877a6179b548df5a8549f46826de9028
christopher rinehart <RInehartC20@gmail.com>
2022-10-30 23:43:38 -0400

    Added Tile prefab to be rendered by the game board on startup, Array of ships

--- Assets/PlayBoard.cs
+++ Assets/PlayBoard.cs
@@ -1,24 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 //creates the game board on startup
 public class PlayBoard : MonoBehaviour
 {
-    // clickable ocean tiles
-    public GameObject tiles;
-    //prefab for tiles
-    public GameObject label;
+    // Ship objects
+    public Ship[] Ships = new Ship[5];
+    // tiles objects
+    public GameObject[] tiles = new GameObject[100];
+    // tile prefab
+    public GameObject tilePrefab;
+    // tile parent
+    public Transform tileParent;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for(int x = 0; x < 10; x++)
+        {
+            for(int y = 0; y < 10; y++)
+            {
+                tiles[x*y] = Instantiate(tilePrefab, tileParent);
+                (tiles[x*y].transform as RectTransform).anchoredPosition = new Vector2(40*x,40*y);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    // shot miss
+    private void Miss()
+    {
+        Debug.Log("Miss");
     }
 }
