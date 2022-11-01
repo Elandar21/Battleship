7e6087d1fbbb447a5f354e0c89a5e4c68f54db5c
christopher rinehart <RInehartC20@gmail.com>
2022-11-01 12:00:46 -0400

    Updating individual logic of the board, Setting tiles and checking to see if the boards ships have been sunk

--- Assets/PlayBoard.cs
+++ Assets/PlayBoard.cs
@@ -1,29 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 //creates the game board on startup
 public class PlayBoard : MonoBehaviour
 {
     // Ship objects
     public Ship[] Ships = new Ship[5];
-    // tiles objects
-    public GameObject[] tiles = new GameObject[100];
+    // Tiles objects
+    public GameObject[] Tiles = new GameObject[100];
     // tile prefab
     public GameObject tilePrefab;
     // tile parent
     public Transform tileParent;
+    // ship parent
+    public Transform shipParent;
+    //IsUser
+    public bool IsUserBoard;
+
+    public Action<TileAction> ClickAction;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int x = 0; x < 10; x++)
-        {
-            for(int y = 0; y < 10; y++)
-            {
-                tiles[x*y] = Instantiate(tilePrefab, tileParent);
-                (tiles[x*y].transform as RectTransform).anchoredPosition = new Vector2(40*x,40*y);
-            }
-        }
+        SetTiles();
     }
 
     // Update is called once per frame
@@ -32,9 +32,21 @@ public class PlayBoard : MonoBehaviour
         
     }
 
-    // shot miss
-    private void Miss()
+    //Sets the tiles to the board
+    private void SetTiles()
     {
-        Debug.Log("Miss");
+        for(int y = 0; y < 10; y++)
+        {
+            for(int x = 0; x < 10; x++)
+            {
+                Tiles[y*10+x] = Instantiate(tilePrefab, tileParent);
+                (Tiles[y*10+x].transform as RectTransform).anchoredPosition = new Vector2(40*x,40*y);
+                Button button = Tiles[y*10+x].GetComponent<Button>();
+                if(button != null)
+                {
+                    button.OnClick.AddListener();
+                }
+            }
+        }
     }
 }
