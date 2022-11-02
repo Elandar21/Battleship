34fc153eb9c8b1039d359c3917df325ada1b2beb
christopher rinehart <RInehartC20@gmail.com>
2022-11-01 21:40:42 -0400

    Ability to set interactablity of tiles

--- Assets/Board/Tile.cs
+++ Assets/Board/Tile.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 //Represents the tile object
@@ -6,10 +7,19 @@ public class Tile : MonoBehaviour
 {
     //Allows devices to subscribe to click event
     public Action<Transform> ClickedEvent;
+    //Sets the button interaction
+    public Button button;
 
     //Sends the message up that the tile has been clicked
     public void Clicked()
     {
         ClickedEvent?.Invoke(transform);
+    }
+
+    //Sets the tile to be interactable
+    public void SetInteractable(bool isInteractable)
+    {
+        if(button != null)
+            button.interactable = isInteractable;
     }
 }
